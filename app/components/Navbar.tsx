692b5e6bd615e716d44f69ed030b315f191c514a
Erhandoe <erhandoe@gmail.com>
2025-05-26 12:03:02 +0200

    Error fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { LoginButton } from "./LoginButton";
 import { NavList } from "./NavList";
 
 export const Navbar = async () => {
-  const str = logString();
+  const str = await logString();
   return (
     <div className="font-orbitron flex row-auto center items-center p-3 pr-8 pl-8 space-x-10 bg-gray-800 rounded-xl m-auto mt-2 w-fit border-blue-600 border-2 shadow-blue-400 shadow-md sticky top-0">
       <NavList />
